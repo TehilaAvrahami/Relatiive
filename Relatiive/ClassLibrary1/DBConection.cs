9380d267a1c10a95705e833240586fdcd05827f0
TehilaAvrahami <92549187+TehilaAvrahami@users.noreply.github.com>
2021-12-22 23:49:11 +0200

    מחלקות גנריות

---
+++ Relatiive/ClassLibrary1/DBConection.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using System.Collections.Generic;
+
+namespace ClassLibrary1
+{
+    public class DBConection
+    {
+        public DBConection()
+        {
+        }
+
+        public List<T> GetDbSet<T>() where T : class
+        {
+            using (musicCompositionEntities musicCompEntity = new musicCompositionEntities())
+            {
+                return musicCompEntity.GetDbSet<T>().ToList();
+            }
+        }
+
+        public enum ExecuteActions
+        {
+            Insert,
+            Update,
+            Delete
+        }
+
+        public void Execute<T>(T entity, ExecuteActions exAction) where T : class
+        {
+            using (musicCompositionEntities musicCompEntity = new musicCompositionEntities())
+            {
+                var model = musicCompEntity.GetCollection<T>();
+                switch (exAction)
+                {
+                    case ExecuteActions.Insert:
+                        model.InsertOne(entity);
+                        break;
+                    case ExecuteActions.Update:
+                        model.Attach(entity);
+                        musicCompEntity.Entry(entity).State = System.Data.Entity.EntityState.UpdateOne;
+                        break;
+                    case ExecuteActions.Delete:
+                        model.Attach(entity);
+                        musicCompEntity.Entry(entity).State = System.Data.Entity.EntityState.DeleteOne;
+                        break;
+                    default:
+                        break;
+                }
+                musicCompEntity.SaveChanges();
+
+            }
+        }
+    }
+}
---
+++ Relatiive/ClassLibrary1/musicCompositionEntities.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    internal class musicCompositionEntities
+    {
+        internal object GetDbSet<T>() where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        internal object GetCollection<T>() where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        internal object Entry<T>(T entity) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
