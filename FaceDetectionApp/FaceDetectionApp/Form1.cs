1b96734d5916afeb4bddf5816e1fcffab1cb1cb9
TehilaAvrahami <tehila213i@gmail.com>
2022-01-20 17:13:28 +0200

    ert

---
+++ FaceDetectionApp/FaceDetectionApp/Form1.cs
@@ -0,0 +1,27 @@
+﻿using Emgu.CV;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FaceDetectionApp
+{
+    public partial class Form1 : Form
+    {
+        static readonly CascadeClassifier cascadeClassifier = new CascadeClassifier("haarcascade_frontalface_alt_tree.xml");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
