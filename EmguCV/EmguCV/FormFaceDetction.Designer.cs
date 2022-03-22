e72eec21ae822aeeabbe6adb070c778a4b7f1fac
TehilaAvrahami <tehila213i@gmail.com>
2022-03-22 19:11:07 +0200

    22/03 new

---
+++ EmguCV/EmguCV/FormFaceDetction.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace EmguCV
+{
+    partial class FormFaceDetction
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.oToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.detectFaceHaarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.detectFaceLBPToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(699, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(699, 510);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.oToolStripMenuItem,
+            this.detectFaceHaarToolStripMenuItem,
+            this.detectFaceLBPToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // oToolStripMenuItem
+            // 
+            this.oToolStripMenuItem.Name = "oToolStripMenuItem";
+            this.oToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.oToolStripMenuItem.Text = "Open";
+            this.oToolStripMenuItem.Click += new System.EventHandler(this.oToolStripMenuItem_Click);
+            // 
+            // detectFaceHaarToolStripMenuItem
+            // 
+            this.detectFaceHaarToolStripMenuItem.Name = "detectFaceHaarToolStripMenuItem";
+            this.detectFaceHaarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.detectFaceHaarToolStripMenuItem.Text = "Detect Face Haar";
+            // 
+            // detectFaceLBPToolStripMenuItem
+            // 
+            this.detectFaceLBPToolStripMenuItem.Name = "detectFaceLBPToolStripMenuItem";
+            this.detectFaceLBPToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.detectFaceLBPToolStripMenuItem.Text = "Detect Face LBP";
+            // 
+            // FormFaceDetction
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(699, 534);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FormFaceDetction";
+            this.Text = "FormFaceDetction";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem oToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem detectFaceHaarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem detectFaceLBPToolStripMenuItem;
+    }
+}
---
+++ EmguCV/EmguCV/FormFaceDetction.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Emgu.CV;
+using Emgu.CV.
+namespace EmguCV
+{
+    public partial class FormFaceDetction : Form
+    {
+        public FormFaceDetction()
+        {
+            InitializeComponent();
+        }
+
+        private void oToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
