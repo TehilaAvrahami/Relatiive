f6393c346f97c2271204b103b282a9d5ccd7d88e
TehilaAvrahami <tehila213i@gmail.com>
2022-04-27 15:33:31 +0300

    real time

--- Client/Rilatiive/src/app/app-routing.module.ts
+++ Client/Rilatiive/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { RealTimeComponent } from './components/real-time/real-time.component';
 import { SignInComponent } from './components/sign-in/sign-in.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 
@@ -10,7 +11,8 @@ const routes: Routes = [
   { path: 'Home',component:HomeComponent},
   { path: 'SignUp',component:SignUpComponent},
   { path: 'SignIn',component:SignInComponent},
-  
+  { path: 'Real',component:RealTimeComponent},
+
   
 ];
 
