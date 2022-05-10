61dd2202b8da67174dac04a1148d69fee6d5a2ba
TehilaAvrahami <tehila213i@gmail.com>
2022-05-10 16:21:29 +0300

    הביתהההההההה

--- Client/Rilatiive/src/app/components/home/home.component.ts
+++ Client/Rilatiive/src/app/components/home/home.component.ts
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  imageLogo="assets/big2.png"
+  // imageApp="assets/image.png"
+  im1 ="assets/1.jpg"
+  im2 ="assets/2.jpg"
+  im3 ="assets/3.jpg"
+  im4 ="assets/4.jpg"
 
   constructor() { }
 
