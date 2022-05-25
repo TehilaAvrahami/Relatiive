d0e25a32988ea96b7f5f27901a45875585c36c43
TehilaAvrahami <tehila213i@gmail.com>
2022-05-25 14:19:00 +0300

    בבבב

--- Relatiive/BL/FaceDetect.cs
+++ Relatiive/BL/FaceDetect.cs
@@ -10,24 +10,4 @@ using Emgu.CV.Structure;
 
 namespace BL
 {
-    public class FaceDetect
-    {
-        public static Image<Bgr, byte> imgInput;
-
-        static string faceDetect(string filename)
-        {
-            imgInput = new Image<Bgr, byte>(filename);
-            Console.WriteLine("imgInput" + imgInput);
-            string facePath = Path.GetFullPath(@"../../data/haarcascade_frontalface_default.xml");
-            CascadeClassifier classfierFace = new CascadeClassifier(facePath);
-            var imgGray = imgInput.Convert<Gray, byte>().Clone();
-            Rectangle[] faces = classfierFace.DetectMultiScale(imgGray, 1.1, 4);
-            foreach (var face in faces)
-            {
-                imgInput.Draw(face, new Bgr(0, 0, 255), 2);
-            }
-            //pictureBox1.Image = imgInput.ToBitmap(300, 300);
-            return filename;
-        }
-    }
 }
